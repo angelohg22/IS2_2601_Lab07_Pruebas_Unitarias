84c582cbc5c00059c55163db8d5c5d6764441803
Juler Anderson <juleranderson@gmail.com>
2026-06-12 18:28:06 -0500

    style: format code in GestorTareas class for consistency

--- src/main/java/GestorTareas.java
+++ src/main/java/GestorTareas.java
@@ -3,31 +3,31 @@ import java.util.List;
 
 public class GestorTareas {
 
-  private List<String> tareasPendientes = new ArrayList<>();
-  private List<String> tareasCompletadas = new ArrayList<>();
+    private List<String> tareasPendientes = new ArrayList<>();
+    private List<String> tareasCompletadas = new ArrayList<>();
 
-  public void agregarTarea(String tarea) {
-    tareasPendientes.add(tarea);
-  }
-
-  public void completarTarea(String tarea) {
-    if (tareasPendientes.remove(tarea)) {
-      tareasCompletadas.add(tarea);
-    } else {
-      throw new IllegalArgumentException("Tarea no encontrada");
+    public void agregarTarea(String tarea) {
+        tareasPendientes.add(tarea);
     }
-  }
 
-  public void eliminarTarea(String tarea) {
-    tareasPendientes.remove(tarea);
-    tareasCompletadas.remove(tarea);
-  }
+    public void completarTarea(String tarea) {
+        if (tareasPendientes.remove(tarea)) {
+            tareasCompletadas.add(tarea);
+        } else {
+            throw new IllegalArgumentException("Tarea no encontrada");
+        }
+    }
 
-  public List<String> getPendientes() {
-    return tareasPendientes;
-  }
+    public void eliminarTarea(String tarea) {
+        tareasPendientes.remove(tarea);
+        tareasCompletadas.remove(tarea);
+    }
 
-  public List<String> getCompletadas() {
-    return tareasCompletadas;
-  }
+    public List<String> getPendientes() {
+        return tareasPendientes;
+    }
+
+    public List<String> getCompletadas() {
+        return tareasCompletadas;
+    }
 }
